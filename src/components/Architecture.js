d79ac8efab8128531bd5c41139271de591e1faed
monicamuroruiz <monicamuroportfolio@gmail.com>
2025-03-12 16:25:45 -0600

    Flipbook images
    Fix to flipbook images

--- src/components/Architecture.js
+++ src/components/Architecture.js
@@ -5,50 +5,50 @@ import './Flipbook.css';
 
 // Correct image imports (assuming these are in the src folder)
 
-import img1 from '../images/resolucion/res/portresolucion.jpg';
-import img2 from '../images/resolucion/res/portresolucion2.jpg';
-import img3 from '../images/resolucion/res/portresolucion3.jpg';
-import img4 from '../images/resolucion/res/portresolucion4.jpg';
-import img5 from '../images/resolucion/res/portresolucion5.jpg';
-import img6 from '../images/resolucion/res/portresolucion6.jpg';
-import img7 from '../images/resolucion/res/portresolucion7.jpg';
-import img8 from '../images/resolucion/res/portresolucion8.jpg';
-import img9 from '../images/resolucion/res/portresolucion9.jpg';
-import img10 from '../images/resolucion/res/portresolucion10.jpg';
-import img11 from '../images/resolucion/res/portresolucion11.jpg';
-import img12 from '../images/resolucion/res/portresolucion12.jpg';
-import img13 from '../images/resolucion/res/portresolucion13.jpg';
-import img14 from '../images/resolucion/res/portresolucion14.jpg';
-import img15 from '../images/resolucion/res/portresolucion15.jpg';
-import img16 from '../images/resolucion/res/portresolucion16.jpg';
-import img17 from '../images/resolucion/res/portresolucion17.jpg';
-import img18 from '../images/resolucion/res/portresolucion18.jpg';
-import img19 from '../images/resolucion/res/portresolucion19.jpg';
-import img20 from '../images/resolucion/res/portresolucion20.jpg';
-import img21 from '../images/resolucion/res/portresolucion21.jpg';
-import img22 from '../images/resolucion/res/portresolucion22.jpg';
-import img23 from '../images/resolucion/res/portresolucion23.jpg';
-import img24 from '../images/resolucion/res/portresolucion24.jpg';
-import img25 from '../images/resolucion/res/portresolucion25.jpg';
-import img26 from '../images/resolucion/res/portresolucion26.jpg';
-import img27 from '../images/resolucion/res/portresolucion27.jpg';
-import img28 from '../images/resolucion/res/portresolucion28.jpg';
-import img29 from '../images/resolucion/res/portresolucion29.jpg';
-import img30 from '../images/resolucion/res/portresolucion30.jpg';
-import img31 from '../images/resolucion/res/portresolucion31.jpg';
-import img32 from '../images/resolucion/res/portresolucion32.jpg';
-import img33 from '../images/resolucion/res/portresolucion33.jpg';
-import img34 from '../images/resolucion/res/portresolucion34.jpg';
-import img35 from '../images/resolucion/res/portresolucion35.jpg';
-import img36 from '../images/resolucion/res/portresolucion36.jpg';
-import img37 from '../images/resolucion/res/portresolucion37.jpg';
-import img38 from '../images/resolucion/res/portresolucion38.jpg';
-import img39 from '../images/resolucion/res/portresolucion39.jpg';
-import img40 from '../images/resolucion/res/portresolucion40.jpg';
-import img41 from '../images/resolucion/res/portresolucion41.jpg';
-import img42 from '../images/resolucion/res/portresolucion42.jpg';
-import img43 from '../images/resolucion/res/portresolucion43.jpg';
-import img44 from '../images/resolucion/res/portresolucion44.jpg';
+import img1 from '../images/flipbook/bookjpg';
+import img2 from '../images/flipbook/book2.jpg';
+import img3 from '../images/flipbook/book3.jpg';
+import img4 from '../images/flipbook/book4.jpg';
+import img5 from '../images/flipbook/book5.jpg';
+import img6 from '../images/flipbook/book6.jpg';
+import img7 from '../images/flipbook/book7.jpg';
+import img8 from '../images/flipbook/book8.jpg';
+import img9 from '../images/flipbook/book9.jpg';
+import img10 from '../images/flipbook/book10.jpg';
+import img11 from '../images/flipbook/book11.jpg';
+import img12 from '../images/flipbook/book12.jpg';
+import img13 from '../images/flipbook/book13.jpg';
+import img14 from '../images/flipbook/book14.jpg';
+import img15 from '../images/flipbook/book15.jpg';
+import img16 from '../images/flipbook/book16.jpg';
+import img17 from '../images/flipbook/book17.jpg';
+import img18 from '../images/flipbook/book18.jpg';
+import img19 from '../images/flipbook/book19.jpg';
+import img20 from '../images/flipbook/book20.jpg';
+import img21 from '../images/flipbook/book21.jpg';
+import img22 from '../images/flipbook/book22.jpg';
+import img23 from '../images/flipbook/book23.jpg';
+import img24 from '../images/flipbook/book24.jpg';
+import img25 from '../images/flipbook/book25.jpg';
+import img26 from '../images/flipbook/book26.jpg';
+import img27 from '../images/flipbook/book27.jpg';
+import img28 from '../images/flipbook/book28.jpg';
+import img29 from '../images/flipbook/book29.jpg';
+import img30 from '../images/flipbook/book30.jpg';
+import img31 from '../images/flipbook/book31.jpg';
+import img32 from '../images/flipbook/book32.jpg';
+import img33 from '../images/flipbook/book33.jpg';
+import img34 from '../images/flipbook/book34.jpg';
+import img35 from '../images/flipbook/book35.jpg';
+import img36 from '../images/flipbook/book36.jpg';
+import img37 from '../images/flipbook/book37.jpg';
+import img38 from '../images/flipbook/book38.jpg';
+import img39 from '../images/flipbook/book39.jpg';
+import img40 from '../images/flipbook/book40.jpg';
+import img41 from '../images/flipbook/book41.jpg';
+import img42 from '../images/flipbook/book42.jpg';
+import img43 from '../images/flipbook/book43.jpg';
+import img44 from '../images/flipbook/book44.jpg';
 
 
 
@@ -63,6 +63,7 @@ img6 ,
 img7 ,
 img8 ,
 img9 ,
+img10 ,
 img11,
 img12,
 img13,
